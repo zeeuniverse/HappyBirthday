a69d85d89da866964f1fbc144093c2e7a9b20302
Naz <raheelnaz@raheels-mbp.lan>
2021-10-10 16:10:27 -0400

    add fake urls

--- app/src/main/java/networkadapter/NetworkAdapter.kt
+++ app/src/main/java/networkadapter/NetworkAdapter.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 object NetworkAdapter {
     val retrofit: Retrofit = Retrofit
         .Builder()
-        .baseUrl("fake URL")
+        .baseUrl("fake Ug")
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 }
