09e22f6a551587c7bc167190754bce704c7bf135
Naz <raheelnaz@raheels-mbp.lan>
2021-10-14 19:34:05 -0400

    add bday message

--- app/src/main/java/com/zia/happybirthday/MainActivity.kt
+++ app/src/main/java/com/zia/happybirthday/MainActivity.kt
@@ -2,10 +2,23 @@ package com.zia.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.LazyThreadSafetyMode.NONE
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val textView: TextView by lazy(NONE) { findViewById(R.id.my_text_view) }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        textView.text = "Happy Birthday"
+
+
     }
 }
